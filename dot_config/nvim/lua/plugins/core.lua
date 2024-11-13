5500dd94207a96b626990c88738bcef6d78b0343
hvpaiva <highlander.paiva@mercadolivre.com>
2024-11-12 23:56:09 -0300

    chore: updated nvim plugins

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -1,54 +1,52 @@
 return {
-  -- Generic log syntax highlighting and filetype management for Neovim
-  {
-    "fei6409/log-highlight.nvim",
-    lazy = true,
-    event = "BufRead *.log",
-    opts = {},
-  },
-  -- Establish good command workflow and quit bad habit
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = true,
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-      disable_mouse = false,
-    },
-    keys = {
-      { "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"' },
-      { "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"' },
-    },
-  },
-  -- NeoVim plugin with which you can track the time you spent on files, projects, repos, filetypes
-  {
-    "Dronakurl/usage-tracker.nvim",
-    config = function()
-      require("usage-tracker").setup({
-        json_file = vim.fn.stdpath("data") .. "/usage-tracker.json",
-        keep_eventlog_days = 365,
-        cleanup_freq_days = 356,
-      })
-    end,
-  },
-  -- Prismatic line decorations for the adventurous vim user
-  {
-    "mvllow/modes.nvim",
-    tag = "v0.2.0",
-    config = function()
-      require("modes").setup({
-        colors = {
-          bg = "#82aaff",
-          copy = "#86e1fc",
-          delete = "#ff757f",
-          insert = "#c3e88d",
-          visual = "#c099ff",
-        },
-        line_opacity = 0.25,
-        set_cursor = true,
-        set_cursorline = true,
-        set_number = true,
-        ignore_filetypes = { "NvimTree", "TelescopePrompt" },
-      })
-    end,
-  },
+	-- Generic log syntax highlighting and filetype management for Neovim
+	{
+		"fei6409/log-highlight.nvim",
+		lazy = true,
+		event = "BufRead *.log",
+		opts = {},
+	},
+	-- Establish good command workflow and quit bad habit
+	{
+		"m4xshen/hardtime.nvim",
+		opts = {
+			disable_mouse = false,
+		},
+		keys = {
+			{ "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"' },
+			{ "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"' },
+		},
+	},
+	-- NeoVim plugin with which you can track the time you spent on files, projects, repos, filetypes
+	{
+		"Dronakurl/usage-tracker.nvim",
+		config = function()
+			require("usage-tracker").setup({
+				json_file = vim.fn.stdpath("data") .. "/usage-tracker.json",
+				keep_eventlog_days = 365,
+				cleanup_freq_days = 356,
+			})
+		end,
+	},
+	-- Prismatic line decorations for the adventurous vim user
+	{
+		"mvllow/modes.nvim",
+		tag = "v0.2.0",
+		config = function()
+			require("modes").setup({
+				colors = {
+					bg = "#82aaff",
+					copy = "#86e1fc",
+					delete = "#ff757f",
+					insert = "#c3e88d",
+					visual = "#c099ff",
+				},
+				line_opacity = 0.25,
+				set_cursor = true,
+				set_cursorline = true,
+				set_number = true,
+				ignore_filetypes = { "NvimTree", "TelescopePrompt" },
+			})
+		end,
+	},
 }
--- dot_config/nvim/lua/plugins/overrides.lua
+++ dot_config/nvim/lua/plugins/overrides.lua
@@ -1,81 +1,81 @@
 -- these plugins are already installed but include some configuration changes.
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    opts = {
-      style = "night",
-      on_colors = function(colors)
-        colors.border = "#0db9d7"
-      end,
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      preset = "helix",
-    },
-  },
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        separator_style = "slant",
-      },
-    },
-  },
-  {
-    "folke/noice.nvim",
-    opts = {
-      presets = {
-        bottom_search = false,
-        lsp_doc_border = true,
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        border = "rounded",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      ---@type vim.diagnostic.Opts
-      diagnostics = {
-        underline = true,
-        update_in_insert = true,
-        virtual_text = false,
-        float = {
-          border = "rounded",
-        },
-      },
-      codelens = {
-        enabled = true,
-      },
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-      })
-      opts.window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      }
-    end,
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "tokyonight",
+		},
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		opts = {
+			style = "night",
+			on_colors = function(colors)
+				colors.border = "#0db9d7"
+			end,
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		opts = {
+			preset = "helix",
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				separator_style = "slant",
+			},
+		},
+	},
+	{
+		"folke/noice.nvim",
+		opts = {
+			presets = {
+				bottom_search = false,
+				lsp_doc_border = true,
+			},
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				border = "rounded",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			---@type vim.diagnostic.Opts
+			diagnostics = {
+				underline = true,
+				update_in_insert = true,
+				virtual_text = false,
+				float = {
+					border = "rounded",
+				},
+			},
+			codelens = {
+				enabled = true,
+			},
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			opts.mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+			})
+			opts.window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			}
+		end,
+	},
 }
